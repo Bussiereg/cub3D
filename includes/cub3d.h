e8af14670e636e555dee6a8cb6a23a7a1cd6b961
julesrb <jbjulesbernard3@gmail.com>
2023-10-09 12:29:15 +0200

    start

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,14 +1,67 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include <fcntl.h>
 # include <stdio.h>
 # include <unistd.h>
-# include <stdlib.h>
-# include <math.h>
 # include "libft.h"
-# include "get_next_line.h"
 # include "MLX42.h"
+# include <stdlib.h>
+# include <errno.h>
 
-void test_func(void);
 
+typedef struct point
+{
+	int		x;
+	int		y;
+	int		z;
+	int		color;
+}	t_point;
+
+typedef struct cub3d
+{
+	int 			res_x;
+	int				res_y;
+
+	mlx_image_t		*minimap;
+	mlx_image_t		*viewport;
+	mlx_t			*mlx;
+
+	int				**map;
+	int				m_size_x;
+	int				m_size_y;
+	
+	char			*text_N;
+	char			*text_S;
+	char			*text_W;
+	char			*text_E;
+	int				color_S;
+	int				color_F;
+
+}	t_cub3d;
+
+/* void	draw_map(t_fdf *fdf);
+int		parse_map(char *file_name, t_map *map);
+void	text_info(mlx_t *mlx, char *argv, t_map *map);
+
+uint8_t	ft_atohex(char *str);
+int		ft_ahextorgba(char *str);
+void	calc_grad_steps(int i, int cl_a, int cl_b, float rgba[4]);
+int		calc_grad_color(int steps, int cl_a, float rgba[4]);
+
+void	draw_line(t_point point_a, t_point point_b, mlx_image_t *fdf);
+void	draw_line_down(t_point a, t_point b, mlx_image_t *fdf);
+void	draw_line_up(t_point a, t_point b, mlx_image_t *fdf);
+
+int		count_split(char **split);
+int		count_map_colums(char *file_name, t_map *map);
+int		count_map_row(char *file_name, t_map *map);
+
+int		free_split(char **split);
+
+void	my_keyhook(mlx_key_data_t keydata, void *param);
+
+void	terminate(char *str);
+void	win_close(void *param);
+int		free_grid(t_grid **grid, t_fdf *fdf); */
 #endif
--- sources/main.c
+++ sources/main.c
@@ -1,14 +1,78 @@
 #include "cub3d.h"
 
-void	test_func(void)
+int	parse_map(char *file_name, t_cub3d *cub3d)
 {
-	ft_printf("hello world\n");
+	int y;
+
+	y = 0;
+	(void)file_name;
+
+	if (ft_strnstr(argv[1], ".cub", ft_strlen(argv[1])) == 0)
+		terminate("Wrong extension !");
+
+	// Read the resolution
+
+	// read the texture info
+	
+	// read and store the RGB color infos
+
+	// find out the size of the map 
+
+	cub3d->m_size_x = 33;
+	cub3d->m_size_y = 14;
+
+	// allocate map
+	
+	cub3d->map = malloc(cub3d->m_size_y * sizeof(int *));
+	// protect and exit correctly
+
+	while (y < cub3d->m_size_y)
+	{
+		cub3d->map[y] = malloc(cub3d->m_size_x * sizeof(int));
+		// protect and exit correctly
+		y++;
+	}
+
 }
 
-int main(int argc, char **argv)
+int	cub3d_init(t_cub3d *cub3d)
 {
-	(void)argc;
-	(void)argv;
-	test_func();
-	return(0);
+	
+
+	cub3d->mlx = mlx_init(1920, 1080, "cub3D", false);
+	if (!cub3d->mlx)
+		terminate("cub3d init error");
+
+	cub3d->viewport = mlx_new_image(cub3d->mlx, 1920, 1080);
+	if (!cub3d->viewport)
+		terminate("cub3d init error");
+
+	// Parse map to get the size of the minimap or always the same size?
+
+	cub3d->minimap = mlx_new_image(cub3d->mlx, 400, 400);
+	if (!cub3d->minimap)
+		terminate("cub3d init error");
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	t_cub3d		cub3d;
+
+	if (argc != 2)
+		terminate("Incorrect number of arguments !");
+
+
+	parse_map(argv[1], &cub3d);
+		//if map problem stop here
+	cub3d_init(&cub3d);
+		//if init problem stop here
+
+
+	draw_minimap(&cub3d);
+	/* mlx_key_hook(fdf->mlx, &my_keyhook, fdf);
+	mlx_close_hook(fdf->mlx, &win_close, fdf); */
+	mlx_loop(cub3d.mlx);
+	mlx_terminate(cub3d.mlx);
+	return (EXIT_SUCCESS);
 }
