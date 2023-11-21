5e6c11a4ce560be0f6d9c767d10d0b39be56a3dc
guillaume <guillaumebussiere.gb@gmail.com>
2023-11-21 11:46:14 +0100

    orientation for texture done

--- sources/draw_viewport.c
+++ sources/draw_viewport.c
@@ -100,7 +100,7 @@ void	draw_game(t_cub3d *cub3d)
 	int stepY;
 	double	perpWallDist;
 	int	hit;
-	int side;
+	char side;
 	int lineHeight;
 	int drawStart;
 	int drawEnd;
@@ -150,19 +150,25 @@ void	draw_game(t_cub3d *cub3d)
 			{
 				cub3d->sideDistX += cub3d->deltaDistX;
 				cub3d->mapX += stepX;
-				side = 0;
+				if (cub3d->rayDirX < 0)
+					side = 'W';
+				else
+					side = 'E';
 			}
 			else
 			{
 				cub3d->sideDistY += cub3d->deltaDistY;
 				cub3d->mapY += stepY;
-				side = 1;
+				if (cub3d->rayDirY < 0)
+					side = 'N';
+				else
+					side = 'S';
 			}
 			if (cub3d->map[cub3d->mapY][cub3d->mapX] == '1') 
 				hit = 1;
 		}
 		// printf("mapX: %d mapY: %d ", cub3d->mapX, cub3d->mapY);
-		if(side == 0) 
+		if(side == 'W' || side == 'E') 
 			perpWallDist = (cub3d->sideDistX - cub3d->deltaDistX);
 		else
 			perpWallDist = (cub3d->sideDistY - cub3d->deltaDistY);
@@ -179,10 +185,14 @@ void	draw_game(t_cub3d *cub3d)
 			drawEnd = HEIGHT - 1;
 		while (drawStart < drawEnd)
 		{
-			if (side == 1)
+			if (side == 'N')
 				mlx_put_pixel(cub3d->viewport, cub3d->ray, drawStart, 0x0000FFFF);
-			else
+			else if (side == 'S')
+				mlx_put_pixel(cub3d->viewport, cub3d->ray, drawStart, 0xFFFFFFFF);
+			else if (side == 'E')
 				mlx_put_pixel(cub3d->viewport, cub3d->ray, drawStart, 0xFF0000FF);
+			else if (side == 'W')
+				mlx_put_pixel(cub3d->viewport, cub3d->ray, drawStart, 0x00FF00FF);
 			drawStart++;
 		}
 		// raycaster(cub3d, ray_angle);
