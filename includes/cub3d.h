e48e8175bf21b97bbf7013f909e3ca25a9e4b69f
guillaume <guillaumebussiere.gb@gmail.com>
2023-11-28 19:47:56 +0100

    drafts sprite

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -14,8 +14,7 @@
 # define CUB3D_H
 
 # include <fcntl.h>
-# include <alsa/asoundlib.h>
-# include <pulse/simple.h>
+// # include <alsa/asoundlib.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <math.h>
@@ -37,6 +36,12 @@ typedef struct point
 	int		y;
 	int		color;
 }	t_point;
+
+typedef struct sprite
+{
+	double x;
+	double y;
+}	t_sprite;
 
 typedef struct cub3d
 {
@@ -69,6 +74,8 @@ typedef struct cub3d
 	mlx_image_t		*minimap;
 	mlx_t			*mlx;
 
+	t_sprite		sprite;
+	
 	char			**map;
 	char			**map_check;
 	int				map_line;
