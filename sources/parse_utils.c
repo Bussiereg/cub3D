076800027100141fedd9376e7440eacdf446d247
Jules <jbjulesbernard3@gmail.com>
2023-12-05 14:31:47 +0100

    norm

--- sources/parse_utils.c
+++ sources/parse_utils.c
@@ -15,7 +15,7 @@
 int	right_map_char(char c)
 {
 	if (c == '0' || c == '1' || c == ' ' || c == 'N' || c == 'S' || c == 'E'
-		|| c == 'W' || c == 'c' || c == 'D')
+		|| c == 'W' || c == 'c' || c == 'D' || c == 'Z')
 		return (1);
 	else
 		return (0);
