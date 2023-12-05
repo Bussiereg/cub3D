de9afa5a2fda0e1740c61dfa13bc33537d9c5e3d
guillaume <guillaumebussiere.gb@gmail.com>
2023-12-05 11:49:31 +0100

    makefile and folders for livrable v2

--- sources_bonus/parse_main_bonus.c
+++ sources_bonus/parse_main_bonus.c
@@ -74,30 +74,70 @@ int	check_wall(char **map, t_cub3d *cub3d)
 	return (0);
 }
 
-void	load_key_text(t_cub3d *cub3d)
+void	load_key_text_1(t_cub3d *cub3d)
 {
-	mlx_texture_t *texture;
+	mlx_texture_t	*texture;
 
 	texture = mlx_load_png("./texture/key-01.png");
+	if (!texture)
+		terminate("key texture file missing", cub3d, 1, 2);
 	cub3d->key1 = mlx_texture_to_image(cub3d->mlx, texture);
+	if (!cub3d->key1)
+		terminate("key texture memory fail", cub3d, 1, 2);
 	mlx_delete_texture(texture);
 	texture = mlx_load_png("./texture/key-02.png");
+	if (!texture)
+		terminate("key texture file missing", cub3d, 1, 2);
 	cub3d->key2 = mlx_texture_to_image(cub3d->mlx, texture);
+	if (!cub3d->key2)
+		terminate("key texture memory fail", cub3d, 1, 2);
 	mlx_delete_texture(texture);
 	texture = mlx_load_png("./texture/key-03.png");
+	if (!texture)
+		terminate("key texture file missing", cub3d, 1, 2);
 	cub3d->key3 = mlx_texture_to_image(cub3d->mlx, texture);
+	if (!cub3d->key3)
+		terminate("key texture memory fail", cub3d, 1, 2);
 	mlx_delete_texture(texture);
+}
+
+void	load_key_text_2(t_cub3d *cub3d)
+{
+	mlx_texture_t	*texture;
+
 	texture = mlx_load_png("./texture/key-04.png");
+	if (!texture)
+		terminate("key texture file missing", cub3d, 1, 2);
 	cub3d->key4 = mlx_texture_to_image(cub3d->mlx, texture);
+	if (!cub3d->key4)
+		terminate("key texture memory fail", cub3d, 1, 2);
 	mlx_delete_texture(texture);
 	texture = mlx_load_png("./texture/key-05.png");
+	if (!texture)
+		terminate("key texture file missing", cub3d, 1, 2);
 	cub3d->key5 = mlx_texture_to_image(cub3d->mlx, texture);
+	if (!cub3d->key5)
+		terminate("key texture memory fail", cub3d, 1, 2);
 	mlx_delete_texture(texture);
 	texture = mlx_load_png("./texture/key-06.png");
+	if (!texture)
+		terminate("key texture file missing", cub3d, 1, 2);
 	cub3d->key6 = mlx_texture_to_image(cub3d->mlx, texture);
+	if (!cub3d->key6)
+		terminate("key texture memory fail", cub3d, 1, 2);
 	mlx_delete_texture(texture);
+}
+
+void	load_key_text_3(t_cub3d *cub3d)
+{
+	mlx_texture_t	*texture;
+
 	texture = mlx_load_png("./texture/key-07.png");
+	if (!texture)
+		terminate("key texture file missing", cub3d, 1, 2);
 	cub3d->key7 = mlx_texture_to_image(cub3d->mlx, texture);
+	if (!cub3d->key7)
+		terminate("key texture memory fail", cub3d, 1, 2);
 	mlx_delete_texture(texture);
 }
 
@@ -118,6 +158,8 @@ int	parse_map(char *file, t_cub3d *cub3d)
 		terminate("no player position", cub3d, 1, 1);
 	duplicate_map(cub3d);
 	check_wall(cub3d->map_check, cub3d);
-	load_key_text(cub3d);
+	load_key_text_1(cub3d);
+	load_key_text_2(cub3d);
+	load_key_text_3(cub3d);
 	return (0);
 }
--- sources_bonus/render_bonus.c
+++ sources_bonus/render_bonus.c
@@ -17,7 +17,8 @@ void	render_background(t_cub3d *cub3d)
 	int	x;
 	int	y;
 
-	mlx_delete_image(cub3d->mlx, cub3d->background);
+	if (cub3d->background)
+		mlx_delete_image(cub3d->mlx, cub3d->background);
 	cub3d->background = mlx_new_image(cub3d->mlx, WIDTH, HEIGHT);
 	y = 0;
 	while (y < HEIGHT)
@@ -48,23 +49,24 @@ void	set_opacity(mlx_image_t *img, int alpha)
 void	render_intro(t_cub3d *cub3d)
 {
 	mlx_texture_t	*texture;
-	int	alpha;
+	int				alpha;
 
 	alpha = 255 - cub3d->frame * 2.5;
-	mlx_delete_image(cub3d->mlx, cub3d->intro);
+	if (cub3d->intro)
+		mlx_delete_image(cub3d->mlx, cub3d->intro);
 	texture = mlx_load_png("./texture/intro.png");
 	cub3d->intro = mlx_texture_to_image(cub3d->mlx, texture);
 	mlx_delete_texture(texture);
 	if (255 - cub3d->frame * 2.5 >= 0)
-	{
 		set_opacity(cub3d->intro, alpha);
-	}
 }
 
 void	render_viewport(t_cub3d *cub3d)
 {
-	mlx_delete_image(cub3d->mlx, cub3d->viewport);
-	mlx_delete_image(cub3d->mlx, cub3d->sprite_img);
+	if (cub3d->viewport)
+		mlx_delete_image(cub3d->mlx, cub3d->viewport);
+	if (cub3d->sprite_img)
+		mlx_delete_image(cub3d->mlx, cub3d->sprite_img);
 	cub3d->viewport = mlx_new_image(cub3d->mlx, WIDTH, GHEIGHT);
 	cub3d->sprite_img = mlx_new_image(cub3d->mlx, WIDTH, GHEIGHT);
 	draw_ceiling(cub3d);
@@ -72,7 +74,6 @@ void	render_viewport(t_cub3d *cub3d)
 	draw_game(cub3d);
 	if (++cub3d->key_frame > 36)
 		cub3d->key_frame = 1;
-	//printf("%d\n", cub3d->key_frame);
 }
 
 void	move_around(t_cub3d *cub3d)
@@ -97,25 +98,22 @@ void	render(void *param)
 
 	cub3d = (t_cub3d *)param;
 	cub3d->frame++;
+	if (key_frame_selector(cub3d) == NULL)
+		ft_printf("frame %d", cub3d->frame);
 	if (cub3d->frame < 255)
 		render_intro(cub3d);
 	move_around(cub3d);
 	render_background(cub3d);
 	render_viewport(cub3d);
 	draw_minimap(cub3d);
-	//background
 	mlx_image_to_window(cub3d->mlx, cub3d->background, 0, 0);
 	mlx_set_instance_depth(cub3d->background->instances, 1);
-	// viewport
 	mlx_image_to_window(cub3d->mlx, cub3d->viewport, 0, 115);
 	mlx_set_instance_depth(cub3d->viewport->instances, 2);
-	// sprite
 	mlx_image_to_window(cub3d->mlx, cub3d->sprite_img, 0, 115);
 	mlx_set_instance_depth(cub3d->sprite_img->instances, 3);
-	// minimap
 	mlx_image_to_window(cub3d->mlx, cub3d->minimap, 0, 0);
 	mlx_set_instance_depth(cub3d->minimap->instances, 4);
-	// intro
 	if (255 - cub3d->frame * 2.5 >= 0)
 	{
 		mlx_image_to_window(cub3d->mlx, cub3d->intro, 0, 0);
