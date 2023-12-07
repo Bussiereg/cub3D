d5a888604083a653d95ffa5d93bed675075991be
Guillaume Bussiere <gbussier@c4b10c1.42berlin.de>
2023-12-07 17:53:57 +0100

    clean up the headers

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -83,59 +83,10 @@ typedef struct cub3d
 	int				rotate_right;
 }	t_cub3d;
 
-// raycaster_main.c
-void		raycaster(t_cub3d *cub3d);
-void		finding_wall(t_cub3d *cub3d);
-void		wall_distance(t_cub3d *cub3d);
-
-// raycaster_calculus.c
-double		absol(double nombre);
-void		raycaster_init(t_cub3d *cub3d);
-void		raycaster_calculus(t_cub3d *cub3d);
-
-// draw_background.c
-int			draw_sky(t_cub3d *cub3d);
-int			draw_ceiling(t_cub3d *cub3d);
-int			draw_floor(t_cub3d *cub3d);
-
-// draw_viewport.c
-int			draw_sky(t_cub3d *cub3d);
-int			draw_ceiling(t_cub3d *cub3d);
-int			draw_floor(t_cub3d *cub3d);
-void		draw_line_textu(double line_height, int text_x_pos,
-				mlx_image_t *text, t_cub3d *cub3d);
-void		draw_game(t_cub3d *cub3d);
-
-/// render.c
-void		render_background(t_cub3d *cub3d);
-void		render_viewport(t_cub3d *cub3d);
-void		render(void *param);
-
-// parse_main.c
-int			copy_map(char *file, t_cub3d *cub3d);
-void		floodfill(t_cub3d *cub3d, int y, int x, char new_val);
-int			check_wall(char **map, t_cub3d *cub3d);
-int			parse_map(char *file, t_cub3d *cub3d);
-
-// parse_info.c
-int			line_to_map(int y, char *line, t_cub3d *cub3d);
-int			map_line(char *line);
-int			info_to_struct(char *line, t_cub3d *cub3d);
-int			all_info_read(t_cub3d *cub3d);
-int			read_info(char *file, t_cub3d *cub3d, char *line, char *trim);
-
-// parse_textures.c
-void		load_textures_2(mlx_texture_t *texture,
-				mlx_image_t **text, t_cub3d *cub3d, char **info);
-void		load_textures(mlx_texture_t	*texture, t_cub3d *cub3d, char **info);
-void		set_player_position(char map_char, int x, int y, t_cub3d *cub3d);
-
-// parse_utlis.c
-int			right_map_char(char c);
-int			read_map_size(char *file, t_cub3d *cub3d, int fd);
-char		**allocate_map(int y, int x);
-int			duplicate_map(t_cub3d *cub3d);
-int			open_file_to_line(char *file, t_cub3d *cub3d);
+// close.c
+void		ft_free_tab(char **tab);
+void		delete_img_cub3d(t_cub3d *cub3d);
+void		terminate(char *str, t_cub3d *cub3d, int mlx, int map);
 
 // color.c
 void		calc_grad_steps(int i, int cl_a, int cl_b, float rgba[4]);
@@ -145,9 +96,22 @@ int			get_color_info(char *str);
 void		draw_pixel(mlx_image_t *img, uint32_t x,
 				uint32_t y, uint32_t color);
 
-// keyhook_rotate.c
-void		rotating_right(t_cub3d *cub3d);
-void		rotating_left(t_cub3d *cub3d);
+// draw_background.c
+int			draw_sky(t_cub3d *cub3d);
+int			draw_ceiling(t_cub3d *cub3d);
+int			draw_floor(t_cub3d *cub3d);
+
+// draw_viewport.c
+void		draw_line_textu(double line_height, int text_x_pos,
+				mlx_image_t *text, t_cub3d *cub3d);
+void		draw_game(t_cub3d *cub3d);
+
+// keyhook_main.c
+void		win_close(void *param);
+void		my_keyhook_move(mlx_key_data_t keydata, t_cub3d	*cub3d);
+void		my_keyhook_rotate(mlx_key_data_t keydata, t_cub3d *cub3d);
+void		my_keyhook_other(mlx_key_data_t keydata, t_cub3d *cub3d);
+void		my_keyhook(mlx_key_data_t keydata, void *param);
 
 // keyhook_moving.c
 void		moving_right(t_cub3d *cub3d);
@@ -156,13 +120,49 @@ void		moving_down(t_cub3d *cub3d);
 void		moving_up(t_cub3d *cub3d);
 void		move_around(t_cub3d *cub3d);
 
-// keyhook_main.c
-void		win_close(void *param);
-void		my_keyhook(mlx_key_data_t keydata, void *param);
+// keyhook_rotate.c
+void		rotating_right(t_cub3d *cub3d);
+void		rotating_left(t_cub3d *cub3d);
 
-// close.c
-void		ft_free_tab(char **tab);
-void		terminate(char *str, t_cub3d *cub3d, int mlx, int map);
-void		delete_img_cub3d(t_cub3d *cub3d);
+// parse_info.c
+int			line_to_map(int y, char *line, t_cub3d *cub3d);
+int			info_to_struct(char *line, t_cub3d *cub3d);
+int			all_info_read(t_cub3d *cub3d);
+int			map_line(char *line);
+int			read_info(char *file, t_cub3d *cub3d, char *line, char *trim);
+
+// parse_main.c
+int			copy_map(char *file, t_cub3d *cub3d);
+void		floodfill(t_cub3d *cub3d, int y, int x, char new_val);
+int			check_wall(char **map, t_cub3d *cub3d);
+int			parse_map(char *file, t_cub3d *cub3d);
+
+// parse_textures.c
+void		load_textures_2(mlx_texture_t *texture,
+				mlx_image_t **text, t_cub3d *cub3d, char **info);
+void		load_textures(mlx_texture_t	*texture, t_cub3d *cub3d, char **info);
+void		set_player_position(char map_char, int x, int y, t_cub3d *cub3d);
+
+// parse_utlis.c
+int			right_map_char(char c);
+int			open_file_to_line(char *file, t_cub3d *cub3d);
+int			read_map_size(char *file, t_cub3d *cub3d, int fd);
+char		**allocate_map(int y, int x);
+int			duplicate_map(t_cub3d *cub3d);
+
+// raycaster_calculus.c
+double		absol(double nombre);
+void		raycaster_init(t_cub3d *cub3d);
+void		raycaster_calculus(t_cub3d *cub3d);
+
+// raycaster_main.c
+void		finding_wall(t_cub3d *cub3d);
+void		wall_distance(t_cub3d *cub3d);
+void		raycaster(t_cub3d *cub3d);
+
+// render.c
+void		render_background(t_cub3d *cub3d);
+void		render_viewport(t_cub3d *cub3d);
+void		render(void *param);
 
 #endif
